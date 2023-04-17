ac26ad1f821184c637bf4225fef153e24bb12a31
Toine MARLANGE <marlange.toine@protonmail.com>
2023-04-17 11:41:19 +0400

    script secured, add supp fonctionnality

--- projet.php
+++ projet.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
-
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,8 +7,8 @@
     <link rel="stylesheet" href="style.css">
     <title>ToDoList</title>
 </head>
-<?php 
-$erreur = "";
+<?php
+$erreurs = "";
 try {
     $bdd = new PDO('mysql:host=localhost;dbname=tâches;charset=utf8', 'root', '');
 } catch (Exception $e) {
@@ -18,45 +17,46 @@ try {
 
 if (isset($_POST['add_task'])) { // On vérifie que la variable POST existe
     if (empty($_POST['add_task'])) {  // On vérifie qu'elle a une valeure
-        $erreurs = 'Vous devez indiquer la valeure de la tâche';
+        $erreurs = 'Vous devez indiquer la valeur de la tâche';
     } else {
         $tache = $_POST['add_task'];
-        $bdd->exec("INSERT INTO todo(task) VALUES('$tache')"); // On insère la tâche dans la base de donnée
+        $requete = $bdd->prepare("INSERT INTO todo (task) VALUES (:task)");
+        $requete->bindParam(':task', $tache);
+        $requete->execute();
     }
 }
 
+if (isset($_GET['supp_task'])) {
+    $id = $_GET['supp_task'];
+    $bdd->exec("DELETE FROM todo WHERE id=$id");
+}
 
 ?>
+
 <body>
     <main>
         <div class="card">
-            <form action="projet.php" method="POST">
-                <div class="card-title">
-                    <h2>To Do List</h2>
-                </div>
-                <div class="card-body">
-                    <input type="text" class="input" name="add_task"><button>Ajouter</button>
-                    <ul>
-                        <?php
-                           if (isset($erreurs)) {
-                        ?>
-                            <p><?php echo $erreurs ?></p>
-                           
-                        <?php
-                           }
-                        $requete = $bdd->query('SELECT task
-                                                FROM todo');
-                        while ($donnees = $requete->fetch()) {
-                        echo '<li><input type="checkbox">'.$donnees['task'].'</li><button>Supp</button>';
-                        };
-                        ?>
-                    </ul>
-                </div>
-            </form>    
+            <div class="card-title">
+                <h2>To Do List</h2>
+            </div>
+            <div class="card-body">
+                <form action="projet.php" method="POST">
+                    <input type="text" class="input" name="add_task">
+                    <button type="submit">Ajouter</button>
+                </form>
+                <?php if (!empty($erreurs)) : ?>
+                    <p><?php echo $erreurs ?></p>
+                <?php endif; ?>
+                <ul>
+                    <?php
+                    $requete = $bdd->query('SELECT id, task FROM todo');
+                    while ($donnees = $requete->fetch()) {
+                        echo '<li><input type="checkbox">' . $donnees['task'] . ' <a href="projet.php?supp_task=' . $donnees['id'] . '">Supp</a></li>';
+                    };
+                    ?>
+                </ul>
+            </div>
         </div>
-
-
     </main>
 </body>
-
 </html>
