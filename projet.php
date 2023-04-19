55bc0b58b7189d7b8b0e0239af540decfde9a6b4
Toine MARLANGE <marlange.toine@protonmail.com>
2023-04-19 10:50:32 +0400

    add modifie functionality, need to work on css, and reduce php code if possible or separate php files

--- projet.php
+++ projet.php
@@ -1,62 +1,97 @@
 <!DOCTYPE html>
-<html lang="fr">
-<head>
+<html lang="en">
+  <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
     <title>ToDoList</title>
-</head>
-<?php
-$erreurs = "";
-try {
-    $bdd = new PDO('mysql:host=localhost;dbname=tâches;charset=utf8', 'root', '');
-} catch (Exception $e) {
-    die('Erreur: ' . $e->getMessage());
-};
+  </head>
+  <body>
+    <?php
+      $erreurs = "";
+      try {
+          $bdd = new PDO('mysql:host=localhost;dbname=tâches;charset=utf8', 'root', '');
+      } catch (Exception $e) {
+          die('Erreur: ' . $e->getMessage());
+      };
+          // AJOUTER UNE TACHE
+      if (isset($_POST['add_task'])) {
+          if (empty($_POST['add_task'])) {
+              $erreurs = 'Vous devez indiquer la valeur de la tâche';
+          } else {
+              $tache = $_POST['add_task'];
+              $requete = $bdd->prepare("INSERT INTO todo (task) VALUES (:task)");
+              $requete->bindParam(':task', $tache);
+              $requete->execute();
+          }
+      }
+        // SUPPRIMER UNE TACHE
+      if (isset($_POST['supp_task'])) {
+          if (empty($_POST['supp_task'])) {
+              $erreurs = 'Vous devez choisir une tâche à suprrimer'; // Peut être à supprimer car supp_task jamais vide ? donc inutile d'avoir un message d'erreur ? 
+          } else {
+              $id = $_POST['supp_task'];
+              $requete = $bdd->prepare("DELETE FROM todo WHERE id=:id");
+              $requete->bindParam(':id', $id);
+              $requete->execute();
+          }
+      }
+        // MODIFIER UNE TACHE
+      if (isset($_POST['edit'])) { // si le bouton edit est cliqué 
+          $id = $_POST['task_id']; 
+          $new_task_name = $_POST['new_task_name'];
+          $requete = $bdd->prepare("UPDATE todo SET task=:task WHERE id=:id"); // on pourrait mettre à la place de :task et :id directement $new_task_name et $id ? mais plus sécurisé grâce a bindparram ?? 
+          $requete->bindParam(':task', $new_task_name);
+          $requete->bindParam(':id', $id);
+          $requete->execute();
+      }
+    ?>
 
-if (isset($_POST['add_task'])) { // On vérifie que la variable POST existe
-    if (empty($_POST['add_task'])) {  // On vérifie qu'elle a une valeure
-        $erreurs = 'Vous devez indiquer la valeur de la tâche';
-    } else {
-        $tache = $_POST['add_task'];
-        $requete = $bdd->prepare("INSERT INTO todo (task) VALUES (:task)");
-        $requete->bindParam(':task', $tache);
-        $requete->execute();
-    }
-}
-
-if (isset($_GET['supp_task'])) {
-    $id = $_GET['supp_task'];
-    $bdd->exec("DELETE FROM todo WHERE id=$id");
-}
-
-?>
-
-<body>
     <main>
-        <div class="card">
-            <div class="card-title">
-                <h2>To Do List</h2>
-            </div>
-            <div class="card-body">
-                <form action="projet.php" method="POST">
-                    <input type="text" class="input" name="add_task">
-                    <button type="submit">Ajouter</button>
-                </form>
-                <?php if (!empty($erreurs)) : ?>
-                    <p><?php echo $erreurs ?></p>
-                <?php endif; ?>
-                <ul>
-                    <?php
-                    $requete = $bdd->query('SELECT id, task FROM todo');
-                    while ($donnees = $requete->fetch()) {
-                        echo '<li><input type="checkbox">' . $donnees['task'] . ' <a href="projet.php?supp_task=' . $donnees['id'] . '">Supp</a></li>';
-                    };
-                    ?>
-                </ul>
-            </div>
+      <div class="card">
+        <div class="card-title">
+          <h2>To Do List</h2>
         </div>
-    </main>
-</body>
-</html>
+        <div class="card-body">
+          <form action="projet.php" method="POST">
+            <input type="text" class="input" name="add_task">
+            <button type="submit">Ajouter</button>
+          </form>
+          <?php if (!empty($erreurs)) : ?>
+            <p><?= $erreurs ?></p>
+          <?php endif; ?>
+          <ul>
+            <?php
+              $requete = $bdd->query('SELECT * FROM todo'); //recupère les données de la table
+              while ($donnees = $requete->fetch()) { // TANT QU'il y a des données affiche ...
+            ?>
+              <li><?= $donnees['task'] ?>
+                <form action="projet.php" method="POST">
+                  <button type="submit" name="supp_task" value="<?= $donnees['id'] ?>">Supprimer</button>
+                </form>
+                <form method="POST">
+                  <input type="hidden" name="task_id" value="<?= $donnees['id'] ?>"> // Utiliser un input hidden pour pouvoir réucuper l'id de la tâche
+                  <button type="submit" name="modifier">Modifier</button>
+                </form>
+                <?php
+                      // RECUPERE LES DONNEES DEJA RENTREES
+                  if (isset($_POST['modifier']) && isset($_POST['task_id'])) { // si le bouton modifier est cliqué et que la donnée à modifié existe ...
+                    $task_id = $_POST['task_id'];
+                    $requete = $bdd->prepare('SELECT * FROM todo WHERE id = :id'); // récupère la donnée correspondante
+                    $requete->bindParam(':id', $task_id);
+                    $requete->execute();
+                    $donnees = $requete->fetch(); // affiche la donnée 
+                    if ($donnees) {
+            ?>
+                <form method="POST">
+                    <input type="text" name="new_task_name" value="<?= $donnees['task'] ?>">
+                    <input type="hidden" name="task_id" value="<?= $donnees['id'] ?>"> // permet de récupéré l'id de la donnée à modifier 
+                    <button type="submit" name="edit">Enregistrer</button>
+                </form>
+            <?php
+                    }
+                  }        // mieux d'utiliser " : " après les if et while pour pour finir par endif endif endwhile ? plus lisible ?
+                }
+            ?>
+
